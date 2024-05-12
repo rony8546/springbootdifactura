ddbd4b74fa01daef84080b0c69ac6f81d58a1b80
ronaldo pacheco <rony8546@gmail.com>
2024-05-11 21:43:44 -0500

    finalizando proyecto, provando anotaciones primery y quality

--- src/main/java/com/ronaldo/springboot/di/factura/springbootdifactura/Appconfig.java
+++ src/main/java/com/ronaldo/springboot/di/factura/springbootdifactura/Appconfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.ronaldo.springboot.di.factura.springbootdifactura.models.Item;
@@ -15,10 +16,21 @@ import com.ronaldo.springboot.di.factura.springbootdifactura.models.Product;
 public class Appconfig {
 
     @Bean
-    List<Item> itemInvoice () {
+    List<Item> itemInvoice() {
         Product p1 = new Product("xbox one", 1200);
         Product p2 = new Product("ps5", 1800);
         return Arrays.asList(new Item(p1, 4), new Item(p2,2)) ;
+    }
+
+    @Bean("default")
+    //@Primary    
+    List<Item> itemInvoiceOffice() {
+        Product p1 = new Product("laptop razer", 2400);
+        Product p2 = new Product("Impresora", 800);
+        Product p3 = new Product("Escritorio", 750);
+        Product p4 = new Product("Monitor Rog asus", 1100);
+        return Arrays.asList(new Item(p1, 3), new Item(p2,1), 
+        new Item(p3,3), new Item(p4,5));
     }
 
 }
--- src/main/java/com/ronaldo/springboot/di/factura/springbootdifactura/models/Invoice.java
+++ src/main/java/com/ronaldo/springboot/di/factura/springbootdifactura/models/Invoice.java
@@ -3,6 +3,7 @@ package com.ronaldo.springboot.di.factura.springbootdifactura.models;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,10 +18,11 @@ public class Invoice {
     @Autowired
     private Client client;
 
-    @Value("${invoice.description}")
+    @Value("${invoice.description.office}")
     private String description;
 
     @Autowired
+    @Qualifier("default")
     private List<Item> items;
 
     public int getTotal () {
